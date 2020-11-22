e67df304ea542952e245e20837f1b94c0fdfc704
louis <louishuguesa@gmail.com>
2020-11-22 22:27:07 +0100

    first commit on windows

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 #add parmelink
 urlpatterns = [
-    #path('', views.index, name='index'),
-    path('', views.index, {'pagename':''}, name='home'),
-    path('<str:pagename>', views.index, name='index')
+    path('', views.index, name='index'),
+    #path('', views.index, {'pagename':''}, name='home'),
+    #path('<str:pagename>', views.index, name='index')
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,13 +4,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    #return HttpResponse("<h1> Homepage</h1>")
-    pagename = '/'+pagename
-    pg = Page.objects.get(permalink=pagename)
-    context =  {
-        'title': pg.title,
-        'content': pg.bodytext,
-        'last_updated': pg.update_date,
+   return HttpResponse("<h1> Homepage</h1>")
+     #pagename = '/'+pagename
+     #pg = Page.objects.get(permalink=pagename)
+     #context =  {
+     #   'title': pg.title,
+     #    'content': pg.bodytext,
+     #    'last_updated': pg.update_date,
 
-    }
-    return render(request, 'pages/page.html',context)
+     #}
+     #return render(request, 'pages/page.html',context)
