fdcc031d078846bde7809ea411a4a4399cf78e57
tsiory <tsiory@1160.local>
2021-01-06 16:43:33 +0100

    test model foreign key

--- ecomapp/models.py
+++ ecomapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -15,5 +16,9 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200)
     description = models.TextField(blank=True,null=True)
     price = models.FloatField()
+
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('detail', args=[str(self.slug)])
--- ecomapp/tests.py
+++ ecomapp/tests.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 # Create your tests here.
 #e l’accès à une base de données pour créer ou interroger des modèles
 from .models import Category, Product
-
+#
+#coverage report
 
 
 class CategoryTestCase(TestCase):
@@ -10,8 +11,20 @@ class CategoryTestCase(TestCase):
         Category.objects.create(title="jouet", slug="jouet")
         Category.objects.create(title="multimedia", slug="multimedia")
 
-    def test_category_have_slug(self):
+    def test_category(self):
         jouet = Category.objects.get(title="jouet")
         multimedia = Category.objects.get(title="multimedia")
         self.assertEqual(str(jouet), "jouet")
         self.assertEqual(str(multimedia), "multimedia")
+
+
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        self.cat = Category.objects.create(title="category1", slug="category1")
+        Product.objects.create(category = self.cat, title="product1", slug="slug_product1", description="descriptions_product1",price = 12 )
+
+    def test_Product(self):
+        dp1 = Product.objects.get(description="descriptions_product1")
+        #print(dp1.get_absolute_url())
+        self.assertEqual(str(dp1), "product1")
